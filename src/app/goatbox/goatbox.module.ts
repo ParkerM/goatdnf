5ac429a88fc2449a4af0d46c7dd838c9f2b270b9
Parker Mauney <parkermauney@gmail.com>
2018-08-24 19:06:17 -0500

    Formatting

--- src/app/goatbox/goatbox.module.ts
+++ src/app/goatbox/goatbox.module.ts
@@ -1,18 +1,26 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatCardModule, MatExpansionModule, MatListModule, MatPaginatorModule, MatTableModule} from '@angular/material';
+import {
+  MatCardModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatListModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
 import {GoatboxComponent} from './goatbox.component';
 import {TldGrabberService} from '../service/tld-grabber.service';
-import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
   imports: [
-    CdkTableModule,
     CommonModule,
     MatCardModule,
     MatExpansionModule,
+    MatInputModule,
     MatListModule,
     MatPaginatorModule,
+    MatSortModule,
     MatTableModule,
   ],
   exports: [
