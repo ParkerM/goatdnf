6c71ca487cc7b42c851802dd72f4f8d09ae7521d
Parker <parkermauney@gmail.com>
2018-09-15 15:21:45 -0500

    Add MatCardModule and remove AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {GoatboxModule} from './goatbox/goatbox.module';
-import {MatToolbarModule} from '@angular/material';
+import {MatCardModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     GoatboxModule,
     HttpClientModule,
+    MatCardModule,
     MatToolbarModule,
   ],
   declarations: [
