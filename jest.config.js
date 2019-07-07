5f45741c48a813b5f1d6a2f3ec7ab0670b0d1d72
Parker Mauney <parkermauney@gmail.com>
2019-07-07 12:23:49 -0500

    Platform and test fixes

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "jest-preset-angular",
   roots: ['src'],
   setupFilesAfterEnv: ["<rootDir>/src/setupJest.ts"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/jestGlobalMocks.ts"
+  ]
 };
